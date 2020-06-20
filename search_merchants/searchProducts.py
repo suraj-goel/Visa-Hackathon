cf19b952acd2ffc1a2bdbdb2556fede0e5da1fe2
sahana63 <49868580+sahana63@users.noreply.github.com>
2020-06-20 20:13:44 +0530

    Update searchProducts.py

--- search_merchants/searchProducts.py
+++ search_merchants/searchProducts.py
@@ -1,7 +1,7 @@
 from builtins import list, len
 
 
-def getSearchResults(mysql,name,merchantid,search_option,filters):
+def getSearchResults(mysql,name,merchantid,search_option,filters,radius):
     cur = mysql.connection.cursor()
     print(filters)
     if search_option=='product':
@@ -39,7 +39,7 @@ def getSearchResults(mysql,name,merchantid,search_option,filters):
             data_res=[]
             for i in data:
                 if i['ProductID'] not in res:
-                    cur.execute("select distinct * from Offer,OfferOnProduct where OfferOnProduct.ProductID=%s and OfferOnProduct.offerID = Offer.offerID and CURDATE()<=ValidTill", (i['ProductID'],))
+                    cur.execute("select distinct * from Product,Offer,OfferOnProduct where OfferOnProduct.ProductID=%s and Product.ProductID = OfferOnProduct.ProductID and  OfferOnProduct.offerID = Offer.offerID and CURDATE()<=ValidTill", (i['ProductID'],))
                     x=list(cur.fetchall())
                     i['Offers']=x
                     res.append(i['ProductID'])
@@ -49,13 +49,22 @@ def getSearchResults(mysql,name,merchantid,search_option,filters):
     else:
         merchant=name
         if filters:
-            cur.execute("SELECT * from Merchant,Product,Offer,OfferOnProduct WHERE Merchant.Name LIKE %s and MerchantID <> %s and Product.ProductID = OfferOnProduct.ProductID and OfferOnProduct.offerID = Offer.offerID and CURDATE()<=ValidTill", ("%"+merchant+"%", merchantid))
+            cur.execute("SELECT distinct Merchant.MerchantID,Merchant.Name from Merchant,Product,Offer,OfferOnProduct WHERE Merchant.Name LIKE %s and Merchant.MerchantID <> %s and Product.MerchantID=Merchant.MerchantID and Product.ProductID = OfferOnProduct.ProductID and OfferOnProduct.offerID = Offer.offerID and CURDATE()<=ValidTill", ("%"+merchant+"%", merchantid))
             data = list(cur.fetchall())
-            return data
         else:
             # add exact matches then based on name then see similarity in name
-            cur.execute("SELECT * from Merchant,Product WHERE Merchant.Name like %s and Product.MerchantID <> %s and Product.MerchantID=Merchant.MerchantID",
-                    ("%"+merchant+"%",merchantid))
+            cur.execute("SELECT * from Merchant WHERE Merchant.Name like %s and Merchant.MerchantID <> %s",("%"+merchant+"%",merchantid))
             data = list(cur.fetchall())
+        data_res = []
+        for i in data:
+            cur.execute("select distinct * from Product,Offer,OfferOnProduct where Product.MerchantID=%s and Product.ProductID = OfferOnProduct.ProductID and OfferOnProduct.offerID = Offer.offerID and CURDATE()<=ValidTill", (i['MerchantID'],))
+            x = list(cur.fetchall())
+            i['Offers'] = x
+            data_res.append(i)
+        print(data_res)
+        return data_res
+
+
+
 
 
