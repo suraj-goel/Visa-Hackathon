6a6c5ac9539c5f13d8de84b46d6a1aba32f24b76
suraj goel <surajgoel.1225@gmail.com>
2020-06-21 15:11:59 +0530

    Fixed app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from search_merchants.searchMerchant import getAllMerchants
 from search_merchants.searchMerchant import getCurrentLocation
 from place_order.displayProduct import displayAllProducts, displayAllOffers
 from search_merchants.searchProducts import getSearchResults
-app=Flask(__name__)
+
 
 app = Flask(__name__,static_folder = '')
 app.jinja_loader = jinja2.ChoiceLoader([app.jinja_loader,jinja2.FileSystemLoader(['.'])])
