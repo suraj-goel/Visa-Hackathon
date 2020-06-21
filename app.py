c314004c4beccbf0470cea6da488eed72f3701e0
Sankalp Dayal <sankalpdayal5@gmail.com>
2020-06-21 13:18:49 +0530

    Show available offers on merchant page

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ import jinja2
 from flask import Flask, render_template, request
 from services.db.db_connection import set_connection
 from search_merchants.searchMerchant import getAllMerchants
-from search_merchants.searchMerchant import getCurrentLocation
 from place_order.displayProduct import displayAllProducts, displayAllOffers
 from search_merchants.searchProducts import getSearchResults
+app=Flask(__name__)
 
 app = Flask(__name__,static_folder = '')
 app.jinja_loader = jinja2.ChoiceLoader([app.jinja_loader,jinja2.FileSystemLoader(['.'])])
+
 
 mysql = set_connection(app)
 
@@ -21,23 +22,23 @@ def showAll():
     # get the currentMerchantID from session.
     currentMerchantID = 2
     data = getAllMerchants(mysql, currentMerchantID)
-    currentLocation = getCurrentLocation(mysql,currentMerchantID)
     if request.method == "POST":
         search_option = request.form['search']
         filter=request.form.get('offerbox')
         radius=request.form['radius']
         product = request.form['name']
         data=getSearchResults(mysql,product, currentMerchantID,search_option,filter, radius)
-        
-        return render_template('./search_merchants/search.html', data=data, product=product ,currentLocation = currentLocation, search_option=search_option)
-    return render_template("./search_merchants/search.html",currentLocation = currentLocation,data=data)
+        return render_template('./search_merchants/search.html', data=data, product=product , search_option=search_option)
+    return render_template("./search_merchants/search.html",data=data)
+
 
 @app.route('/merchant/<merchant_id>')
 def showPlaceOrder(merchant_id):
     currentSelectedMerchantID = merchant_id
     # get the currentSelectedMerchantID from function
     products = displayAllProducts(mysql, currentSelectedMerchantID)
-    return render_template("./place_order/place_order.html", products = products)
+    offers = displayAllOffers(mysql, currentSelectedMerchantID)
+    return render_template("./place_order/place_order.html", products = products, offers = offers)
 
 
 if __name__ == '__main__':
