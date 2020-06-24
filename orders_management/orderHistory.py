813df0396971ab5e07daaa3825c22dc2687e5366
sahana63 <sahanas.cs17@rvce.edu.in>
2020-06-24 22:40:59 +0530

    getOrders

--- orders_management/orderHistory.py
+++ orders_management/orderHistory.py
@@ -1,2 +1,15 @@
 def getOrders(mysql,merchantid):
+    cur=mysql.connection.cursor()
+    cur.execute("select distinct * from Orders, Cart where Orders.CartID=Cart.CartID and Cart.MerchantID="+str(merchantid))
+    carts=cur.fetchall()
+    res=[]
+    for i in carts:
+        data=i
+        cartid=i['CartID']
+        cur.execute("select * from Cart,Product,ProductCart where Product.ProductID=ProductCart.ProductID and ProductCart.CartID=Cart.CartID and Cart.CartID='"+str(cartid)+"';")
+        data['Products_list']=list(cur.fetchall())
+        res.append(data)
+    print(data)
+
+def getRequirements():
     pass
