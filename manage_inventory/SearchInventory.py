a19a2fe62d7dd0bbc1628834a0370081fd9a4286
sahana63 <sahanas.cs17@rvce.edu.in>
2020-06-24 12:18:41 +0530

    inventory

--- manage_inventory/SearchInventory.py
+++ manage_inventory/SearchInventory.py
@@ -1,4 +1,4 @@
-def getAllProducts(mysql,merchantID):
+def getAllProducts(mysql,merchantID,filter='A'):
     cur = mysql.connection.cursor()
     query = "SELECT * FROM Product,Merchant WHERE Product.MerchantID =Merchant.MerchantID AND Merchant.MerchantID = " + str(1)
     cur.execute(query)
