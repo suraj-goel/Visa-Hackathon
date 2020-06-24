9e91a38230e3d6741bb037046d084980170b9967
gaurav3210 <gs070599@gmail.com>
2020-06-24 13:23:04 +0530

    removed error

--- app.py
+++ app.py
@@ -67,6 +67,13 @@ def modify():
 @app.route("/merchant/<merchant_id>/cart",methods=['GET','POST'])
 def showCart(merchant_id):
 	totalQuantity = 0
+	qty = []
+	ProductID = []
+	Name = []
+	Description = []
+	Price = []
+	Offers =  []
+	discountPrice = []
 	if request.method=='GET':
 		try:
 			qty=session['qty']
@@ -76,11 +83,12 @@ def showCart(merchant_id):
 			Price = session['Price']
 			Offers = session['offers']
 			discountPrice = session['discountPrice']
+			l=len(qty)
+			for i in qty:
+				totalQuantity+=int(i)
 		except Exception as e:
 			print("exception details "+str(e))
-		l=len(qty)
-		for i in qty:
-			totalQuantity+=int(i)
+
 
 		return render_template("./place_order/cart.html",merchantID=merchant_id,qty=qty,ProductID=ProductID,Name=Name,Description=Description,Price=Price,Offers=Offers,discountPrice=discountPrice,len=len(qty),totalQuantity=totalQuantity)
 	else:
@@ -99,7 +107,7 @@ def showCart(merchant_id):
 		if(Check==False):
 			addToCart(mysql,qty,ProductID,Name,Description,Price,merchant_id,status,finalPrice,finalDiscountPrice,NegotitatedRequestAmount)
 			modify()
-		session.clear();
+		session.clear()
 		return redirect(url_for('showAll'))
 
 
