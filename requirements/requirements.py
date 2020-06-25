e3f0023986ebb1065096555cb9caf043d8310f98
sahana63 <sahanas.cs17@rvce.edu.in>
2020-06-25 10:55:19 +0530

    change in tabs

--- requirements/requirements.py
+++ requirements/requirements.py
@@ -6,7 +6,7 @@ def saveRequirements(mysql,merchantID,title,description,quantity,price,Item,stat
     if description == "":
         description=Item
     try:
-        cur.execute('insert into Requirement(requirementid, title, description, status, merchantid, price, quantity) VALUES(%s,%s,%s,%s,%s,%s,%s)',(requirementId,title,Item,status,merchantID,price,quantity))
+        cur.execute('insert into Requirement(requirementid, title, description, status, merchantid, price, quantity) VALUES(%s,%s,%s,%s,%s,%s,%s)',(requirementId,title,description,status,merchantID,price,quantity))
         mysql.connection.commit()
     except Exception as e:
         print("problem in inserting in db"+str(e))
