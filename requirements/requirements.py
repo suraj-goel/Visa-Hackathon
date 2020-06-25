0a77a7f098d27062d5db76b1f2b91363f8147aee
gaurav3210 <gs070599@gmail.com>
2020-06-25 12:00:18 +0530

    Modified Create Request and connected with db

--- requirements/requirements.py
+++ requirements/requirements.py
@@ -1,15 +1,22 @@
 import uuid
 
-def saveRequirements(mysql,merchantID,title,description,quantity,price,Item,status):
+def saveRequirements(mysql,merchantID,title,description,quantity,price,status):
     cur = mysql.connection.cursor()
     requirementId = uuid.uuid1()
     if description == "":
-        description=Item
+        description=title
     try:
-        cur.execute('insert into Requirement(requirementid, title, description, status, merchantid, price, quantity) VALUES(%s,%s,%s,%s,%s,%s,%s)',(requirementId,title,Item,status,merchantID,price,quantity))
+        cur.execute('insert into Requirement(requirementid, title, description, status, merchantid, price, quantity) VALUES(%s,%s,%s,%s,%s,%s,%s)',(requirementId,title,description,status,merchantID,price,quantity))
         mysql.connection.commit()
     except Exception as e:
         print("problem in inserting in db"+str(e))
         return None
     print("everything is fine")
     cur.close()
+
+
+def showBusinessName(mysql,merchantID):
+    cur = mysql.connection.cursor()
+    cur.execute("select RegisteredName from Merchant where MerchantID=%s",(str(merchantID)))
+    a = cur.fetchall()
+    return a[0]['RegisteredName']
