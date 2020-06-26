52c8ed9ea1c71ed06b78208daf282306421c3acc
Sankalp Dayal <sankalpdayal5@gmail.com>
2020-06-26 11:33:54 +0530

    Add styles to negotiation

---
+++ negotiation/negotiation.js
@@ -0,0 +1,5 @@
+function showThis(ii) {
+    document.getElementById("thisDiv1").style.display = "none";
+    document.getElementById("thisDiv2").style.display = "none";
+    document.getElementById("thisDiv" + ii).style.display = "block";
+}
