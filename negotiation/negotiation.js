541be81523ec5a91107698a7719aa629b745afc9
Sankalp Dayal <sankalpdayal5@gmail.com>
2020-06-26 12:09:24 +0530

    Add styles and create new negotiation

--- negotiation/negotiation.js
+++ negotiation/negotiation.js
@@ -1,3 +1,9 @@
+$(document).ready(function () {
+    $("#buyerTab").click(function () {
+        $("#createReqTab").toggle();
+    });
+});
+
 function showThis(ii) {
     document.getElementById("thisDiv1").style.display = "none";
     document.getElementById("thisDiv2").style.display = "none";
