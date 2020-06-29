145d8b5770eabb56262e504740309e6590b173bb
suraj goel <surajgoel.1225@gmail.com>
2020-06-29 09:56:03 +0530

    Implemented Address to Coordinates functionality

---
+++ login_registration/addressconversion.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+import googlemaps
+import json
+def getCoordinates(address):
+	
+	gmaps = googlemaps.Client(key='AIzaSyAntxrxhQu11TxFD9wEe7JxxW1UZ0HQXRY')
+	geocode_result = gmaps.geocode(address)
+	
+	return geocode_result[0]['geometry']['location']
+
+
