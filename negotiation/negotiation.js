e0d4a8f8615a59bb489e125b8eaadc6a6a0ee24d
Sankalp Dayal <sankalpdayal5@gmail.com>
2020-07-01 14:44:07 +0530

    Change +/- to text for collapsible

--- negotiation/negotiation.js
+++ negotiation/negotiation.js
@@ -1,10 +1,27 @@
 $(document).ready(function () {
+    //Fixing jQuery Click Events for the iPad
+    var ua = navigator.userAgent,
+        event = (ua.match(/iPad/i)) ? "touchstart" : "click";
+    if ($('.table').length > 0) {
+        $('.table .header').on(event, function () {
+            $(this).toggleClass("active", "").nextUntil('.header').css('display', function (i, v) {
+                return this.style.display === 'table-row' ? 'none' : 'table-row';
+            });
+        });
+    }
+
     $("#buyerTab").click(function () {
         $("#createReqTab").toggle();
     });
 });
 
-
+(() => {
+    let customSelects = document.querySelectorAll(".custom-dropdown__select");
+    customSelects.forEach(el => {
+        if (el.disabled)
+            el.parentNode.classList.add("custom-dropdown--disabled");
+    })
+})();
 
 function showThis(ii) {
     document.getElementById("thisDiv1").style.display = "none";
