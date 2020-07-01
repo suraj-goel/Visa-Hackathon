f11f41649e4686214531331cd4fe39644edc816a
sahana63 <sahanas.cs17@rvce.edu.in>
2020-07-01 10:48:20 +0530

    edit accounts error

--- app.py
+++ app.py
@@ -497,7 +497,8 @@ def editAccountDetails():
 
         if r[2] == 0:
             cur.execute(
-                "update Merchant set Name = '" + name + "', RegisteredName = '" + registeredName + "', EmailID = '" + email + "', ContactNumber = '" + contactno + "', Address = '" + address + "', password = '" + password + "' where MerchantID='" + merchant_id + "';")
+                'update Merchant set Name = "{}", RegisteredName = "{}", EmailID = "{}", ContactNumber = "{}", Address = "{}", password = "{}" where MerchantID="{}";'.format(
+                    name, registeredName, email, contactno, address, password, merchant_id))
             mysql.connection.commit()
             return redirect('/accounts/')
         else:
