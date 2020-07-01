d184ac5b5d1e5f6ea2091ed2bb06c758cf9d2121
suraj goel <surajgoel.1225@gmail.com>
2020-07-01 14:03:32 +0530

    Fixed Distance display in table

--- search_merchants/distanceCoordinates.py
+++ search_merchants/distanceCoordinates.py
@@ -1,11 +1,11 @@
 import math
 def distanceInKMBetweenCoordinates(lat1,long1,lat2,long2):
-    earthRadiusKm = 6371
-    dLat = math.radians(lat2-lat1)
-    dLon = math.radians(long2-long1)
-    lat1 = math.radians(lat1)
-    lat2 = math.radians(lat2)
-    a = math.sin(dLat/2) * math.sin(dLat/2) + math.sin(dLon/2) * math.sin(dLon/2) * math.cos(lat1) * math.cos(lat2)
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-    return earthRadiusKm * c
+    earthRadiusKm =float(6371)
+    dLat = math.radians(float(lat2)-float(lat1))
+    dLon = math.radians(float(long2)-float(long1))
+    lat1 = math.radians(float(lat1))
+    lat2 = math.radians(float(lat2))
+    a = float(math.sin(dLat/2) * math.sin(dLat/2) + math.sin(dLon/2) * math.sin(dLon/2) * math.cos(lat1) * math.cos(lat2))
+    c = float(2 * math.atan2(math.sqrt(a), math.sqrt(1-a)))
+    return float(earthRadiusKm * c)
     
